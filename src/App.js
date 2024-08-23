69f94552ab551038b16bd57c6546ea7b2cf0814a
simranramani1810 <simranramani2003@gmail.com>
2024-08-23 15:42:09 +0530

    App with some features of instagram

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PostForm from "./Components/PostForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Components/PostForm.css";
+import PostList from "./Components/PostList";
+import { useState } from "react";
+import SignIn from "./Components/SignIn";
+import Home from "./Components/Home"
+import "./Components/SignIn.css";
+import Login from "./Components/Login";
+import { BrowserRouter, Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import ProfilePage from "./Components/ProfilePage";
+import PostForHome from "./Components/PostForHome";
+import UserProfile from "./Components/UsersProfile";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(!!localStorage.getItem("token"));
+  const [newPost, setNewPost] = useState(true);
+  //"!!" used to show true & false
+  const navigate = useNavigate
+
+  function updateNewPost() {
+    setNewPost((prev) => !prev);
+  }
+
+  const addPost = (data) => {
+    let storedPost = JSON.parse(localStorage.getItem("posts")) || [];
+    const newPostArray = [...storedPost, data];
+    console.log(newPostArray);
+
+    console.log(data);
+    localStorage.setItem("posts", JSON.stringify(newPostArray));
+  };
+
+  // useEffect(()=> {
+  //   localStorage.getItem("posts")
+  // },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={isLogin?<Navigate to={"/home"}></Navigate>:<Login setIsLogin={setIsLogin}/>}></Route>
+          <Route path="/signup" element={<SignIn/>}></Route>
+          <Route path="/postform" element={<PostForm addPost={addPost} updateNewPost={updateNewPost} />}></Route>
+          <Route path="/postlist" element={<PostList newPost={newPost} updateNewPost={updateNewPost} />}></Route>
+          <Route path="/home" element={isLogin? <Home URL="https://insta-backend-hr3a.onrender.com/allposts" setIsLogin={setIsLogin}/>:<Navigate to={"/signin"}></Navigate>}></Route>
+          <Route path="/profilepage" element={<ProfilePage URL="https://insta-backend-hr3a.onrender.com/myposts"/>}></Route>
+          <Route path="/userprofile" element={<UserProfile></UserProfile>}></Route>
+         </Routes>
+      </BrowserRouter>
     </div>
   );
 }
